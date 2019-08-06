f11f087ed59a9fb1b1f8d9a2f77ec7393811c8e5
shion_chen <qwe753951789@gmail.com>
2019-08-06 17:47:30 +0800

    20190806 New Project

---
+++ Kumatata/Assets/Scripts/MainControl/CircleClick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleClick : MonoBehaviour
+{
+    public GameObject bearFood;
+    Ray ray;
+    RaycastHit hit;
+
+    private void Start()
+    {
+        bearFood = Resources.Load<GameObject>("Prefab/BearFood");
+    }
+
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (Input.GetMouseButtonDown(0) && hit.collider.gameObject.tag == "Circle")
+            {
+                print(hit.collider.name);
+                var cloneBearFood = Instantiate(bearFood, hit.point, Quaternion.identity);
+                cloneBearFood.transform.parent = hit.collider.gameObject.transform;
+            }
+        }
+    }
+}
